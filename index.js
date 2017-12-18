8cf9a484f6b63c6fd0e0fbf48fd8b40a8df2761a
inspiredxstem <srosario2018@bklawtech.com>
2017-12-18 23:31:52 +0000

    done

--- index.js
+++ index.js
@@ -7,6 +7,10 @@
 function List(props) {
     //TODO: Create list from props.
     return (<ul>
+      {
+      props.items.map(function(item, index) {
+        return <li key={index}>{item} <button onClick={props.onClick}>Delete</button> </li>;
+      })}
     </ul>);
 };
 
@@ -14,25 +18,34 @@ var App = React.createClass({
   getInitialState: function() {
     //TODO: Set the initial state.
     return {
-        
+      term: '',
+      items: []  
     };
   },
   onChange: function(event) {
     //TODO: Save current value in input field.
+    this.setState({ term: event.target.value });
   },
   onSubmit: function(event) {
       //TODO: Add to list.
+      event.preventDefault();
+      this.setState({
+      items: [...this.state.items, this.state.term]
+    });
+  },
+  onClick: function(event){
+    console.log(event)
   },
   render: function() {
     //TODO: Add event handlers to input and button.
     return (
       <div>
         <h2>Todo List</h2>
-        <form className="App">
+        <form className="App" onSubmit={this.onSubmit}>
           <input/>
           <button>Add</button>
         </form>
-        <List items={this.state.items} />
+        <List items={this.state.items} onClick={this.onClick} />
       </div>
     );
   }
